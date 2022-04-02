3931d6e1de5290f8032957f91618f27549ede081
vranda-agrawal <vranda.a@simformsolutions.com>
2022-04-02 17:51:20 +0530

    application.rb file setup

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,11 @@ module SnowdenBackendApi
 
     config.middleware.use ActionDispatch::Cookies    
     config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :patch, :put, :delete, :post, :options, :head]
+      end
+    end
   end
 end
